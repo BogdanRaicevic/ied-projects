653e1548e71f0113a7df44855a724d153b1b949c
Bogdan Raicevic <raicevic.bogdan@gmail.com>
2023-01-09 12:22:07 +0100

    Allow writing into input fields when adding a new company

--- src/components/Forms/MyForm/MyForm.tsx
+++ src/components/Forms/MyForm/MyForm.tsx
@@ -2,10 +2,25 @@ import { Button, FormControl, InputAdornment } from "@mui/material";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import { useState } from "react";
+import { IOptionalCompanyData, IOptionalZaposleni } from "../../../fakeData/companyData";
 
 export default function MyForm({ formMetadata, formData }) {
   const [data, setData] = useState(formData);
-  console.log("my data", data);
+
+  function handleChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    setData({
+      ...data,
+      [name]: value,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
 
   const items = formMetadata.map((item) => {
     return (
@@ -15,16 +30,24 @@ export default function MyForm({ formMetadata, formData }) {
           InputProps={{
             startAdornment: <InputAdornment position="start">{item.inputAdornment}</InputAdornment>,
           }}
-          value={data[item.key]}
+          value={data[item.key] || ""}
+          name={item.key}
+          onChange={handleChange}
         />
       </FormControl>
     );
   });
 
   return (
-    <Box component="form" sx={{ display: "flex", flexWrap: "wrap" }} noValidate autoComplete="off">
+    <Box
+      onSubmit={handleSubmit}
+      component="form"
+      sx={{ display: "flex", flexWrap: "wrap" }}
+      noValidate
+      autoComplete="off"
+    >
       {items}
-      <Button sx={{ m: 2, ml: 3, width: "35ch" }} size="large" variant="contained" color="success">
+      <Button sx={{ m: 2, ml: 3, width: "35ch" }} size="large" variant="contained" color="success" type="submit">
         Sačuvaj
       </Button>
     </Box>
