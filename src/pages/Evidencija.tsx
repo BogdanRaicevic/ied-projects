6c7a8ad08e7a76aa8c8d1f32b8ce1e9570ddfab0
Bogdan Raicevic <raicevic.bogdan@gmail.com>
2023-01-19 14:16:49 +0100

    Remove deprecated code

--- src/pages/Evidencija.tsx
+++ src/pages/Evidencija.tsx
@@ -1,8 +1,6 @@
 import { Add } from "@mui/icons-material";
 import { Fab } from "@mui/material";
-import { useMemo, useCallback, useState } from "react";
-import CompanyTable from "../components/CompanyTable";
-import { CompanyTableColumns, ZaposleniTableColumns } from "../components/CompanyTable/TableColumns";
+import { useMemo, useState } from "react";
 import CompanyForm from "../components/Forms/CompanyForm";
 import MyDialog from "../components/MyDialog/MyDialog";
 import PageTitle from "../components/PageTitle";
@@ -10,21 +8,8 @@ import { companiesData } from "../fakeData/companyData";
 import { Company } from "../schemas/companySchemas";
 
 export default function Evidencija() {
-  const columns = useMemo(() => CompanyTableColumns, []);
   const data: Company[] = useMemo(() => companiesData, []);
   const zaposleniInitialState = { hiddenColumns: ["id", "firmaId"] };
-
-  const renderRowSubComponent = useCallback(({ row }: any) => {
-    return (
-      <CompanyTable
-        columns={ZaposleniTableColumns}
-        data={row.values.zaposleni}
-        initialState={zaposleniInitialState}
-        renderRowSubComponent={false}
-      ></CompanyTable>
-    );
-  }, []);
-  const companyInitialState = { hiddenColumns: ["id", "zaposleni"] };
 
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
@@ -39,12 +24,6 @@ export default function Evidencija() {
       <MyDialog open={open} handleClose={handleClose}>
         <CompanyForm></CompanyForm>
       </MyDialog>
-      <CompanyTable
-        columns={columns}
-        data={data}
-        initialState={companyInitialState}
-        renderRowSubComponent={renderRowSubComponent}
-      ></CompanyTable>
     </>
   );
 }
