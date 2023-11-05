80df72bdbfa9ca05d35078f3ecf0156f675990a2
raicevic_bogdan <raicevic.bogdan@gmail.com>
2023-11-05 10:51:49 +0100

    Create indeterminate checkbox component

--- src/pages/Pretrage.tsx
+++ src/pages/Pretrage.tsx
@@ -1,11 +1,14 @@
 import MyTable from "../components/MyTable";
 import PageTitle from "../components/PageTitle";
+import IndeterminateCheckbox from "../components/IndeterminateCheckbox";
 import { companiesData } from "../fakeData/companyData";
 
 export default function Pretrage() {
+  const checkboxOptions = { parent: "bogic", children: ["a", "b", "v"] };
   return (
     <>
       <PageTitle title={"Pretrage"} />
+      <IndeterminateCheckbox options={checkboxOptions} />
       <MyTable data={companiesData}></MyTable>
     </>
   );
