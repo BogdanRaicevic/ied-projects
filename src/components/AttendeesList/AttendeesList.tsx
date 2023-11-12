8ef122c7264ac50b4f93c8abfc6bfcab6a64b3bd
raicevic_bogdan <raicevic.bogdan@gmail.com>
2023-11-12 17:00:38 +0100

    Add ability to remove attendee from the seminar list

--- src/components/AttendeesList/AttendeesList.tsx
+++ src/components/AttendeesList/AttendeesList.tsx
@@ -1,20 +1,34 @@
 import List from "@mui/material/List";
-import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import PersonIcon from "@mui/icons-material/Person";
+import ListItem from "@mui/material/ListItem";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { Button } from "@mui/material";
+import { useState } from "react";
 
 export default function AttendeesList(props: { ucesnici: string[] }) {
+  const [ucesnici, setUcesnici] = useState(props.ucesnici);
+
+  const handleRemoveItem = (index: number) => {
+    const newUcesnici = [...ucesnici];
+    newUcesnici.splice(index, 1);
+    setUcesnici(newUcesnici);
+  };
+
   return (
     <List sx={{ width: "100%", bgcolor: "background.paper", mt: 2, mb: 2 }} component="nav">
-      {props.ucesnici.map((ucesnik, index) => {
+      {ucesnici.map((ucesnik, index) => {
         return (
-          <ListItemButton key={index}>
+          <ListItem key={index}>
             <ListItemIcon>
               <PersonIcon />
             </ListItemIcon>
             <ListItemText primary={ucesnik} />
-          </ListItemButton>
+            <Button onClick={() => handleRemoveItem(index)} sx={{ color: "darkred" }}>
+              <DeleteIcon />
+            </Button>
+          </ListItem>
         );
       })}
     </List>
