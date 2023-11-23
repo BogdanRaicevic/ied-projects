7f1bb082dfe54a855f0b08c0c9aa42d691f81762
raicevic_bogdan <raicevic.bogdan@gmail.com>
2023-11-23 21:49:10 +0100

    Fix selection text to be visible

--- src/components/SelectSeminar/SelectSeminar.tsx
+++ src/components/SelectSeminar/SelectSeminar.tsx
@@ -3,17 +3,24 @@ import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import { fakeSeminars } from "../../fakeData/seminarsData";
+import { useState } from "react";
 
-export default function SelectSeminar({ onSeminarSelect }: { onSeminarSelect: (seminarId: string) => void }) {
+export default function SelectSeminar({
+  onSeminarSelect,
+}: {
+  onSeminarSelect: (seminarId: string) => void;
+}) {
+  const [seminar, setSeminar] = useState<string>("");
   const handleChange = (event: SelectChangeEvent<{ value: unknown }>) => {
     console.log("item: ", event.target.value);
+    setSeminar(event.target.value as string);
     onSeminarSelect(event.target.value as string);
   };
 
   return (
     <FormControl fullWidth>
       <InputLabel>Izaberi i dodaj Seminar</InputLabel>
-      <Select value={""} label={"Izaberi Seminar"} onChange={handleChange}>
+      <Select value={seminar as any} label={"Izaberi Seminar"} onChange={handleChange}>
         {fakeSeminars.map((seminar) => (
           <MenuItem value={seminar.id} key={seminar.id}>
             {seminar.datum} - {seminar.naziv}
