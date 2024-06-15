782af57be572e376be75f1a7b1c802db5c8cb790
Bogdan Raicevic <bogdan.raicevic@symphony.is>
2024-06-15 19:09:47 +0200

    Set up nodemon

--- ied-be/src/server.ts
+++ ied-be/src/server.ts
@@ -1,14 +1,14 @@
-import { env } from './utils/envVariables';
-import { connectDB } from './database/db';
-import express from 'express';
-import cors from 'cors';
+import { env } from "./utils/envVariables";
+import { connectDB } from "./database/db";
+import express from "express";
+import cors from "cors";
 
 const app = express();
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === "development") {
   app.use(
     cors({
-      origin: '*', // Allow all origins in development
+      origin: "*", // Allow all origins in development
       credentials: true, // Reflect CORS headers in responses
     })
   );
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === 'development') {
   // Replace '*' with your specific allowed origins
   app.use(
     cors({
-      origin: ['http://localhost:8000', 'https://localhost:5173'],
+      origin: ["http://localhost:8000", "https://localhost:5173"],
       credentials: true,
     })
   );
@@ -29,10 +29,10 @@ async function initServer() {
     await connectDB();
     await app.listen({ port: Number(env.be.appPort) });
   } catch (error) {
-    console.error('Error starting server:', error);
+    console.error("Error starting server:", error);
   }
 }
 
 initServer()
-  .then(() => console.log('Server is up'))
+  .then(() => console.log("Server is up"))
   .catch(console.error);
