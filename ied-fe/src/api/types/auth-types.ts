743718b21c1b933219c8d0ae5409583ef55729e8
raicevic_bogdan <raicevic.bogdan@gmail.com>
2024-07-03 22:52:46 +0200

    Remove deprecated code

--- ied-fe/src/api/types/auth-types.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const LoginSchema = z.object({
-  email: z.string(), // TODO: add .email() to validate email
-  password: z.string(), // TODO: add .min(8) to validate password
-});
-
-export type LoginSchema = z.infer<typeof LoginSchema>;
