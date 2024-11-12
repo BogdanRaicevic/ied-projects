59edab263be41126fd938d9bab3844469b282f0d
raicevic_bogdan <raicevic.bogdan@gmail.com>
2024-11-12 22:54:00 +0100

    Read query parameters as optional parameters

--- ied-be/src/utils/firmaQueryBuilder.ts
+++ ied-be/src/utils/firmaQueryBuilder.ts
@@ -27,7 +27,7 @@ export type FirmaQueryParams = {
 export function createFirmaQuery(params: FirmaQueryParams): FilterQuery<FirmaType> {
   const query: FilterQuery<FirmaType> = {};
 
-  const negations = params.negacije || [];
+  const negations = params?.negacije || [];
   const negateRadnoMesto = negations.includes(Negations.RadnoMesto);
   const negateTipFirme = negations.includes(Negations.TipFirme);
   const negateDelatnost = negations.includes(Negations.Delatnost);
@@ -35,19 +35,19 @@ export function createFirmaQuery(params: FirmaQueryParams): FilterQuery<FirmaTyp
 
   // console.log("ovo su params: ", params);
   // add stanja firme to search
-  if (params.imeFirme && params.imeFirme.length > 0) {
+  if (params?.imeFirme && params.imeFirme.length > 0) {
     query.naziv_firme = { $regex: params.imeFirme, $options: "i" }; // Case-insensitive partial match
   }
 
-  if (params.pib && params.pib.length > 0) {
+  if (params?.pib && params.pib.length > 0) {
     query.PIB = { $regex: params.pib, $options: "i" }; // Case-insensitive partial match
   }
 
-  if (params.email && params.email.length > 0) {
+  if (params?.email && params.email.length > 0) {
     query.e_mail = { $regex: params.email, $options: "i" }; // Case-insensitive partial match
   }
 
-  if (Array.isArray(params.delatnosti) && params.delatnosti.length > 0) {
+  if (Array.isArray(params?.delatnosti) && params.delatnosti.length > 0) {
     if (negateDelatnost) {
       query.delatnost = { $nin: params.delatnosti };
     } else {
@@ -55,7 +55,7 @@ export function createFirmaQuery(params: FirmaQueryParams): FilterQuery<FirmaTyp
     }
   }
 
-  if (Array.isArray(params.mesta) && params.mesta.length > 0) {
+  if (Array.isArray(params?.mesta) && params.mesta.length > 0) {
     if (negateMesto) {
       query.mesto = { $nin: params.mesta };
     } else {
@@ -63,7 +63,7 @@ export function createFirmaQuery(params: FirmaQueryParams): FilterQuery<FirmaTyp
     }
   }
 
-  if (Array.isArray(params.tipoviFirme) && params.tipoviFirme.length > 0) {
+  if (Array.isArray(params?.tipoviFirme) && params.tipoviFirme.length > 0) {
     if (negateTipFirme) {
       query.tip_firme = { $nin: params.tipoviFirme };
     } else {
@@ -71,7 +71,7 @@ export function createFirmaQuery(params: FirmaQueryParams): FilterQuery<FirmaTyp
     }
   }
 
-  if (Array.isArray(params.radnaMesta) && params.radnaMesta.length > 0) {
+  if (Array.isArray(params?.radnaMesta) && params.radnaMesta.length > 0) {
     if (negateRadnoMesto) {
       query.zaposleni = { $elemMatch: { radno_mesto: { $nin: params.radnaMesta } } };
     } else {
@@ -79,23 +79,23 @@ export function createFirmaQuery(params: FirmaQueryParams): FilterQuery<FirmaTyp
     }
   }
 
-  if (Array.isArray(params.velicineFirmi) && params.velicineFirmi.length > 0) {
+  if (Array.isArray(params?.velicineFirmi) && params.velicineFirmi.length > 0) {
     query.velicina = { $in: params.velicineFirmi };
   }
 
-  if (Array.isArray(params.stanjaFirme) && params.stanjaFirme.length > 0) {
+  if (Array.isArray(params?.stanjaFirme) && params.stanjaFirme.length > 0) {
     query.stanje_firme = { $in: params.stanjaFirme };
   }
 
-  if (params.jbkjs && params.jbkjs.length > 0) {
+  if (params?.jbkjs && params.jbkjs.length > 0) {
     query.jbkjs = { $regex: params.jbkjs, $options: "i" }; // Case-insensitive partial match
   }
 
-  if (params.maticniBroj && params.maticniBroj.length > 0) {
+  if (params?.maticniBroj && params.maticniBroj.length > 0) {
     query.maticni_broj = { $regex: params.maticniBroj, $options: "i" }; // Case-insensitive partial match
   }
 
-  if (params.komentar && params.komentar.length > 0) {
+  if (params?.komentar && params.komentar.length > 0) {
     query.komentar = { $regex: params.komentar.replace(/\s+/g, "\\s*"), $options: "is" };
   }
 
