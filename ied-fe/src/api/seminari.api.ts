f0d60d32c1e516dc8a94ec965991986a835d4af9
raicevic_bogdan <raicevic.bogdan@gmail.com>
2024-11-16 20:50:37 +0100

    Remove comment

--- ied-fe/src/api/seminari.api.ts
+++ ied-fe/src/api/seminari.api.ts
@@ -3,7 +3,6 @@ import { env } from "../utils/envVariables";
 
 export const saveSeminar = async (naziv: string, predavac: string, lokacija: string) => {
   try {
-    console.log("this is current url", `${env.beURL}/api/seminari/save`);
     const response = await axios.post(`${env.beURL}/api/seminari/save`, {
       naziv,
       predavac,
