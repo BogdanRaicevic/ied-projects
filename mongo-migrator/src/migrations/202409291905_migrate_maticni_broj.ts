fe59c2c1499880ba23b46d036e284528c71c9ed3
Bogdan Raicevic <bogdan.raicevic@symphony.is>
2024-12-24 13:52:26 +0100

    Fix tiny bug when PIB is empty

--- mongo-migrator/src/migrations/202409291905_migrate_maticni_broj.ts
+++ mongo-migrator/src/migrations/202409291905_migrate_maticni_broj.ts
@@ -1,8 +1,8 @@
-import { mongoDbConnection } from '../config';
+import { mongoDbConnection } from "../config";
 
 export const up = async () => {
   const mongoDb = await mongoDbConnection();
-  const mongoCollectionName = 'firmas';
+  const mongoCollectionName = "firmas";
 
   try {
     const mongoCollection = mongoDb.collection(mongoCollectionName);
@@ -12,21 +12,21 @@ export const up = async () => {
     while (await cursor.hasNext()) {
       const document: any = await cursor.next();
 
-      let oldPib = (document.PIB as string).toLowerCase();
-      if (oldPib.includes('mb:')) {
-        let pibParts = oldPib.split('mb:');
-        let pibPart = pibParts[0].trim().replace(/,/g, '');
+      let oldPib = ((document?.PIB as string) || "").toLowerCase();
+      if (oldPib.includes("mb:")) {
+        let pibParts = oldPib.split("mb:");
+        let pibPart = pibParts[0].trim().replace(/,/g, "");
         let mbPart = pibParts[1].trim();
 
         let numberRegex = /^\d*$/;
 
         if (!numberRegex.test(pibPart) || !numberRegex.test(mbPart)) {
           console.error(
-            'Invalid pib or mb, company name: ',
+            "Invalid pib or mb, company name: ",
             document.naziv_firme,
-            'pib:',
+            "pib:",
             pibPart,
-            'mb:',
+            "mb:",
             mbPart
           );
           continue;
@@ -41,6 +41,6 @@ export const up = async () => {
       }
     }
   } catch (error) {
-    console.error('Error during migration of maticni broj:', error);
+    console.error("Error during migration of maticni broj:", error);
   }
 };
