433c79cb0305baecc4d7f4d90f5023924f26936e
raicevic_bogdan <raicevic.bogdan@gmail.com>
2025-01-08 17:04:56 +0100

    Update migration rules

--- mongo-migrator/src/migrations/202412261828_fix_bad_emails.ts
+++ mongo-migrator/src/migrations/202412261828_fix_bad_emails.ts
@@ -1,39 +1,41 @@
 import { mongoDbConnection } from "../config";
 
 export const up = async () => {
-  const mongoDb = await mongoDbConnection();
-  const mongoCollectionName = "firmas";
+	const mongoDb = await mongoDbConnection();
+	const mongoCollectionName = "firmas";
 
-  try {
-    const mongoCollection = mongoDb.collection(mongoCollectionName);
+	try {
+		const mongoCollection = mongoDb.collection(mongoCollectionName);
 
-    const cursor = mongoCollection.find();
+		const cursor = mongoCollection.find();
 
-    while (await cursor.hasNext()) {
-      const document = await cursor.next();
+		while (await cursor.hasNext()) {
+			const document = await cursor.next();
 
-      const zaposleni: any[] = document?.zaposleni || [];
+			const zaposleni: any[] = document?.zaposleni || [];
 
-      // Update emails in place
-      zaposleni.forEach(z => {
-        z.e_mail = z.e_mail?.toLowerCase()
-          .replace('č', 'c')
-          .replace('ć', 'c')
-          .replace('š', 's')
-          .replace('ž', 'z')
-          .replace('đ', 'd')
-          .replace(' ', '');
-      });
+			// Update emails in place
+			for (const z of zaposleni) {
+				z.e_mail = z.e_mail
+					?.toLowerCase()
+					.replace("č", "c")
+					.replace("ć", "c")
+					.replace("š", "s")
+					.replace("ž", "z")
+					.replace("đ", "d")
+					.replace(",", ".")
+					.replace(" ", "");
+			}
 
-      await mongoCollection.updateOne(
-        {
-          _id: document!._id,
-        },
-        { $set: { zaposleni: zaposleni } }
-      );
-    }
-  } catch (error) {
-    console.error("Error during migration of fix bad emails:", error);
-    throw error;
-  }
+			await mongoCollection.updateOne(
+				{
+					_id: document?._id,
+				},
+				{ $set: { zaposleni: zaposleni } },
+			);
+		}
+	} catch (error) {
+		console.error("Error during migration of fix bad emails:", error);
+		throw error;
+	}
 };
