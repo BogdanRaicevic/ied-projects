1c6f89ed3b7f12a94e6b6aae05847e9e04bc4161
raicevic_bogdan <raicevic.bogdan@gmail.com>
2025-02-01 15:21:39 +0100

    Fix lint error

--- ied-be/src/utils/utils.ts
+++ ied-be/src/utils/utils.ts
@@ -1,2 +1,2 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+// biome-ignore lint/suspicious:noExplicitAny: This is global ANY that will be removed later in a refactor
 export type TODO_ANY = any;
