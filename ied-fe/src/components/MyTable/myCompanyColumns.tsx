5a15929606d013b54486ff65b846e19984eabdb3
raicevic_bogdan <raicevic.bogdan@gmail.com>
2025-02-04 21:37:22 +0100

    Add migration to use `velicina_firme` and fix bug related to it

--- ied-fe/src/components/MyTable/myCompanyColumns.tsx
+++ ied-fe/src/components/MyTable/myCompanyColumns.tsx
@@ -5,121 +5,124 @@ import { Icon, Link } from "@mui/material";
 import BusinessIcon from "@mui/icons-material/Business";
 
 export const myCompanyColumns: MRT_ColumnDef<FirmaType>[] = [
-  // {
-  //   header: "Prijavljeni",
-  //   accessorKey: "zeleMarketingMaterijal",
-  //   muiTableBodyCellProps: ({ cell }) => ({
-  //     sx: {
-  //       backgroundColor: cell.getValue() === true ? "#47e147" : "salmon",
-  //     },
-  //   }),
-  //   Cell: ({ cell }) => <span>{cell.getValue<boolean>() ? "DA" : "Ne"}</span>,
-  // },
-  {
-    header: "Naziv kompanije",
-    accessorKey: "naziv_firme",
-    Cell: ({ row }: { row: { original: FirmaType } }) => {
-      const firma = row.original;
-      return (
-        <Link
-          href={`/Firma/${firma._id}`}
-          target="_blank"
-          rel="noopener noreferrer"
-          sx={{ display: "inline-flex", alignItems: "center" }}
-        >
-          <Icon component={BusinessIcon} sx={{ fontSize: 20, marginRight: 0.5 }} />
-          {firma.naziv_firme}
-        </Link>
-      );
-    },
-  },
-  {
-    header: "Email",
-    accessorKey: "e_mail",
-    enableClickToCopy: true,
-    muiCopyButtonProps: {
-      fullWidth: true,
-      startIcon: <ContentCopy />,
-      sx: { justifyContent: "flex-start" },
-    },
-  },
+	// {
+	//   header: "Prijavljeni",
+	//   accessorKey: "zeleMarketingMaterijal",
+	//   muiTableBodyCellProps: ({ cell }) => ({
+	//     sx: {
+	//       backgroundColor: cell.getValue() === true ? "#47e147" : "salmon",
+	//     },
+	//   }),
+	//   Cell: ({ cell }) => <span>{cell.getValue<boolean>() ? "DA" : "Ne"}</span>,
+	// },
+	{
+		header: "Naziv kompanije",
+		accessorKey: "naziv_firme",
+		Cell: ({ row }: { row: { original: FirmaType } }) => {
+			const firma = row.original;
+			return (
+				<Link
+					href={`/Firma/${firma._id}`}
+					target="_blank"
+					rel="noopener noreferrer"
+					sx={{ display: "inline-flex", alignItems: "center" }}
+				>
+					<Icon
+						component={BusinessIcon}
+						sx={{ fontSize: 20, marginRight: 0.5 }}
+					/>
+					{firma.naziv_firme}
+				</Link>
+			);
+		},
+	},
+	{
+		header: "Email",
+		accessorKey: "e_mail",
+		enableClickToCopy: true,
+		muiCopyButtonProps: {
+			fullWidth: true,
+			startIcon: <ContentCopy />,
+			sx: { justifyContent: "flex-start" },
+		},
+	},
 
-  {
-    header: "Adresa",
-    accessorKey: "adresa",
-  },
-  {
-    header: "Mesto",
-    accessorKey: "mesto",
-  },
-  {
-    header: "Postanski broj",
-    accessorKey: "postanski_broj",
-  },
-  {
-    header: "Telefon",
-    accessorKey: "telefon",
-  },
-  {
-    header: "Tip firme",
-    accessorKey: "tip_firme",
-  },
-  {
-    header: "Velicina",
-    accessorKey: "velicina",
-  },
-  {
-    header: "Komentari",
-    accessorKey: "komentar",
-    muiTableHeadCellProps: {
-      sx: {
-        minWidth: "400px",
-      },
-    },
-    muiTableBodyCellProps: {
-      sx: {
-        whiteSpace: "pre-wrap", // Preserve line breaks and whitespace
-      },
-    },
-  },
+	{
+		header: "Adresa",
+		accessorKey: "adresa",
+	},
+	{
+		header: "Mesto",
+		accessorKey: "mesto",
+	},
+	{
+		header: "Postanski broj",
+		accessorKey: "postanski_broj",
+	},
+	{
+		header: "Telefon",
+		accessorKey: "telefon",
+	},
+	{
+		header: "Tip firme",
+		accessorKey: "tip_firme",
+	},
+	{
+		header: "Velicina firme",
+		accessorKey: "velicina_firme",
+	},
+	{
+		header: "Komentari",
+		accessorKey: "komentar",
+		muiTableHeadCellProps: {
+			sx: {
+				minWidth: "400px",
+			},
+		},
+		muiTableBodyCellProps: {
+			sx: {
+				whiteSpace: "pre-wrap", // Preserve line breaks and whitespace
+			},
+		},
+	},
 ];
 
 export const myZaposleniColumns: MRT_ColumnDef<Zaposleni>[] = [
-  {
-    header: "Ime i Prezime",
-    accessorFn: (row) => `${row.ime} ${row.prezime}`,
-  },
-  {
-    header: "Email",
-    accessorKey: "e_mail",
-    enableClickToCopy: true,
-    muiCopyButtonProps: {
-      fullWidth: true,
-      startIcon: <ContentCopy />,
-      sx: { justifyContent: "flex-start" },
-    },
-  },
-  {
-    header: "Telefon",
-    accessorKey: "telefon",
-  },
-  {
-    header: "Radna mesta",
-    accessorKey: "radno_mesto",
-    accessorFn: (row) => row.radno_mesto,
-  },
-  {
-    header: "Komentari",
-    accessorKey: "komentar",
-    muiTableHeadCellProps: {
-      sx: {
-        minWidth: "400px",
-      },
-    },
-    muiTableBodyCellProps: {
-      sx: {
-        whiteSpace: "pre-wrap", // Preserve line breaks and whitespace
-      },
-    },
-  },
+	{
+		header: "Ime i Prezime",
+		accessorFn: (row) => `${row.ime} ${row.prezime}`,
+	},
+	{
+		header: "Email",
+		accessorKey: "e_mail",
+		enableClickToCopy: true,
+		muiCopyButtonProps: {
+			fullWidth: true,
+			startIcon: <ContentCopy />,
+			sx: { justifyContent: "flex-start" },
+		},
+	},
+	{
+		header: "Telefon",
+		accessorKey: "telefon",
+	},
+	{
+		header: "Radna mesta",
+		accessorKey: "radno_mesto",
+		accessorFn: (row) => row.radno_mesto,
+	},
+	{
+		header: "Komentari",
+		accessorKey: "komentar",
+		muiTableHeadCellProps: {
+			sx: {
+				minWidth: "400px",
+			},
+		},
+		muiTableBodyCellProps: {
+			sx: {
+				whiteSpace: "pre-wrap", // Preserve line breaks and whitespace
+			},
+		},
+	},
 ];
---
+++ mongo-migrator/src/migrations/202502042127_change_velicina_to_velicina_firme.ts
@@ -0,0 +1,27 @@
+import { mongoDbConnection } from "../config";
+
+export const up = async () => {
+	const mongoDb = await mongoDbConnection();
+	const collectionsToUpdate = ["firmas", "velicine_firmi"];
+
+	try {
+		for (const collectionName of collectionsToUpdate) {
+			const mongoCollection = mongoDb.collection(collectionName);
+			const cursor = mongoCollection.find();
+
+			while (await cursor.hasNext()) {
+				const document = await cursor.next();
+
+				if (document?.velicina !== undefined) {
+					await mongoCollection.updateOne(
+						{ _id: document._id },
+						{ $rename: { velicina: "velicina_firme" } },
+					);
+				}
+			}
+		}
+	} catch (error) {
+		console.error("Error during migration of fix bad emails:", error);
+		throw error;
+	}
+};
