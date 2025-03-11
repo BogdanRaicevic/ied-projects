80f1340d89de7a5a0d2d06404c9c553a93a71d1e
raicevic_bogdan <raicevic.bogdan@gmail.com>
2025-03-12 00:33:19 +0100

    Try fixing pm2 error related to allowed files to be served

--- ied-fe/vite.config.ts
+++ ied-fe/vite.config.ts
@@ -1,15 +1,19 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	build: {
-		outDir: "dist",
-		sourcemap: false,
-		target: "esnext",
-	},
-	server: {
-		allowedHosts: ["ied-baza.xyz", "bs-baza.xyz"],
-	},
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+    target: "esnext",
+  },
+  server: {
+    allowedHosts: ["ied-baza.xyz", "bs-baza.xyz"],
+    fs: {
+      allow: [path.resolve(__dirname), path.resolve(__dirname, "..")],
+    },
+  },
 });
