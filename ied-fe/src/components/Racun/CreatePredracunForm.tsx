f57e8e51217511d160f8e962651111adc4c55bf8
raicevic_bogdan <raicevic.bogdan@gmail.com>
2025-04-01 22:44:28 +0200

    Allow manually adding pozivNaBroj

--- ied-fe/src/components/Racun/CreatePredracunForm.tsx
+++ ied-fe/src/components/Racun/CreatePredracunForm.tsx
@@ -1,4 +1,4 @@
-import { Grid2, Divider, Typography } from "@mui/material";
+import { Grid2, Divider, Typography, TextField } from "@mui/material";
 import { forwardRef, useImperativeHandle, useCallback, useEffect } from "react";
 import { PrimalacRacunaSection } from "./components/PrimalacRacunaSection";
 import { OnlinePrisustvaSection } from "./components/OnlinePrisustvaSection";
@@ -53,7 +53,14 @@ export const CreatePredracunForm = forwardRef<RacunFormRef, RacunFormProps>(
       <Grid2 container>
         <Grid2 size={12}>
           <Typography align="center" variant="h4" sx={{ mb: 3 }}>
-            Predračun __Poziv na broj: __
+            Predračun{" "}
+            <TextField
+              name="pozivNaBroj"
+              value={racun.pozivNaBroj}
+              size="small"
+              sx={{ width: "150px", ml: 1 }}
+              onChange={handleRacunChange}
+            />
           </Typography>
           <PrimalacRacunaSection racun={racun} onRacunChange={handleRacunChange} />
           <Divider sx={{ mt: 3, mb: 3 }} />
