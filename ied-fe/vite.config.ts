c107114f9e4272284db7b4be0cf8653c13b914e3
raicevic_bogdan <raicevic.bogdan@gmail.com>
2025-04-06 22:55:18 +0200

    Handle vite configuration error, no access to .env file

--- ied-fe/vite.config.ts
+++ ied-fe/vite.config.ts
@@ -1,5 +1,5 @@
 import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
+import { defineConfig, searchForWorkspaceRoot } from "vite";
 import path from "path";
 
 // https://vitejs.dev/config/
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     allowedHosts: ["ied-baza.xyz", "bs-baza.xyz"],
     fs: {
-      allow: [path.resolve(__dirname), path.resolve(__dirname, "..")],
+      allow: [path.resolve(__dirname), searchForWorkspaceRoot(process.cwd())],
     },
   },
   resolve: {
