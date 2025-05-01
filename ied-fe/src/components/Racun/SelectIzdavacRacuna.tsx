b1ee91e75adad569f359156fa0676c8962d84268
raicevic_bogdan <raicevic.bogdan@gmail.com>
2025-05-01 23:15:22 +0200

    Reset tekuci racun when izdavac changes

--- ied-fe/src/components/Racun/SelectIzdavacRacuna.tsx
+++ ied-fe/src/components/Racun/SelectIzdavacRacuna.tsx
@@ -1,5 +1,5 @@
 import MenuItem from "@mui/material/MenuItem/MenuItem";
-import Select from "@mui/material/Select/Select";
+import Select, { SelectChangeEvent } from "@mui/material/Select/Select";
 import iedLogo from "../../images/ied-logo.png";
 import permanentLogo from "../../images/permanent-logo.png";
 import bsLogo from "../../images/bs-logo.png";
@@ -15,12 +15,14 @@ export default function SelectIzdavacRacuna() {
   const selectedIzdavac = useRacunStore((state) => state.racunData.izdavacRacuna);
   const updateField = useRacunStore((state) => state.updateField);
 
+  const handleChange = (event: SelectChangeEvent<string>) => {
+    const selectedValue = event.target.value as string;
+    updateField("izdavacRacuna", selectedValue);
+    updateField("tekuciRacun", "");
+  };
+
   return (
-    <Select
-      sx={{ maxWidth: 300, maxHeight: 70 }}
-      value={selectedIzdavac}
-      onChange={(e) => updateField("izdavacRacuna", e.target.value as string)}
-    >
+    <Select sx={{ maxWidth: 300, maxHeight: 70 }} value={selectedIzdavac} onChange={handleChange}>
       {options.map((opt) => (
         <MenuItem key={opt.id} value={opt.id}>
           <img src={opt.logo} style={{ maxWidth: "200px", width: "100%" }} />
