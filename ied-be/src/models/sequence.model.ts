f79d2fc5fa8c18748b1e29998a970d8aea274d68
raicevic_bogdan <raicevic.bogdan@gmail.com>
2025-05-23 11:18:27 +0200

    Cleanup 2

--- ied-be/src/models/sequence.model.ts
+++ ied-be/src/models/sequence.model.ts
@@ -1,12 +1,13 @@
 import { Document, model, Schema } from "mongoose";
 
+type SequenceModelType = Document & {
+  _id: string;
+  sequenceNumber: number;
+};
+
 const sequenceSchema = new Schema({
   _id: { type: String, required: true },
   sequenceNumber: { type: Number, default: 0 },
 });
 
-export const SequenceModel = model("Sequence", sequenceSchema);
-export type SequenceModelType = Document & {
-  _id: string;
-  sequenceNumber: number;
-};
+export const SequenceModel = model<SequenceModelType>("Sequence", sequenceSchema);
