3400dda7407a27ec8256502cba0f5fa080abb548
Bogdan Raicevic <bogdan.raicevic@symphony.is>
2025-08-06 16:15:37 +0200

    Show audit with diff

--- ied-fe/src/pages/AuditLog.tsx
+++ ied-fe/src/pages/AuditLog.tsx
@@ -1,16 +1,22 @@
 import type { AuditLogType } from "@ied-shared/types/audit_log.zod"; // Adjust the import path as necessary
-import { diffJson } from "diff";
 import {
   MaterialReactTable,
   type MRT_ColumnDef,
   useMaterialReactTable,
 } from "material-react-table";
 import { useMemo } from "react";
+import { AuditChangesViewer } from "../components/AuditChangesViewer/AuditChangesViewer";
 import { useAuditLogs } from "../hooks/useAuditLogs";
+import { generateStructuredDiff } from "../utils/diffGenerator";
 
 export default function AuditLog() {
-  const { data: auditLogs, isLoading } = useAuditLogs();
-  console.log("Audit Logs:", auditLogs);
+  const { data, isLoading } = useAuditLogs();
+
+  const {
+    data: auditLogs,
+    totalDocuments,
+    totalPages,
+  } = data || { auditLogs: [], totalDocuments: 0, totalPages: 0 };
 
   const auditLogsColumns = useMemo<MRT_ColumnDef<AuditLogType>[]>(
     () => [
@@ -27,57 +33,23 @@ export default function AuditLog() {
         header: "Tip resursa",
       },
       {
-        accessorFn: (row) => row.resource?.id,
+        accessorKey: "resource.id",
         header: "ID resursa",
       },
       {
-        header: "Changes",
-        // Cell: ({ row }) => {
-        //   const { beforeChanges, requestBody } = row.original;
+        header: "Promene",
+        minSize: 500,
+        Cell: ({ row }) => {
+          const { before, after } = row.original;
 
-        //   // If there's no 'before' state, it's a new creation. Show the whole body as added.
-        //   if (!beforeChanges) {
-        //     return (
-        //       <pre style={{ margin: 0, backgroundColor: "#ddfadd" }}>
-        //         {JSON.stringify(requestBody, null, 2)}
-        //       </pre>
-        //     );
-        //   }
+          const changes = useMemo(() => generateStructuredDiff(before, after), [before, after]);
 
-        //   // Create a filtered version of beforeChanges that only includes
-        //   // keys that are present in the requestBody
-        //   const filteredBeforeChanges = requestBody
-        //     ? Object.keys(requestBody).reduce(
-        //         (filtered, key) => {
-        //           // Only copy fields that exist in requestBody
-        //           if (key in beforeChanges) {
-        //             filtered[key] = beforeChanges[key];
-        //           }
-        //           return filtered;
-        //         },
-        //         {} as Record<string, any>,
-        //       )
-        //     : beforeChanges;
+          if (!changes) {
+            return null;
+          }
 
-        //   const differences = diffJson(filteredBeforeChanges, requestBody);
-
-        //   return (
-        //     <pre style={{ margin: 0, whiteSpace: "pre-wrap", wordBreak: "break-all" }}>
-        //       {differences
-        //         .filter((part) => part.added || part.removed)
-        //         .map((part, index) => {
-        //           const style = {
-        //             backgroundColor: part.added ? "#ddfadd" : "#fadddd",
-        //           };
-        //           return (
-        //             <span key={index} style={style}>
-        //               {part.value}
-        //             </span>
-        //           );
-        //         })}
-        //     </pre>
-        //   );
-        // },
+          return <AuditChangesViewer changes={changes} />;
+        },
       },
     ],
     [],
