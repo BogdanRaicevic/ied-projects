3380847a8ca70d8e3f33b5d7408665377f5871d6
Bogdan Raicevic <raicevic.bogdan@gmail.com>
2025-11-25 23:39:21 +0100

    chore: show loading animation when switching tabs

--- ied-fe/src/pages/Seminari.tsx
+++ ied-fe/src/pages/Seminari.tsx
@@ -1,7 +1,7 @@
 import type { SeminarQueryParams } from "@ied-shared/types/seminar.zod";
-import { Box, Tab, Tabs } from "@mui/material";
+import { Box, Skeleton, Tab, Tabs } from "@mui/material";
 import { addMonths, subMonths } from "date-fns";
-import { useState } from "react";
+import { useState, useTransition } from "react";
 import { ParametriPretrageSeminar } from "../components/Seminari/ParametriPretrageSeminar";
 import SeminariTable from "../components/Seminari/SeminariTable";
 export default function Seminari() {
@@ -14,30 +14,44 @@ export default function Seminari() {
       datumDo: addMonths(new Date(), 3),
     });
   const [tabIndex, setTabIndex] = useState(0);
+  const [isPending, startTransition] = useTransition();
+
   const handlePretraziSeminare = (values: SeminarQueryParams) => {
     setTableInputParameters(values);
   };
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setTabIndex(newValue);
+    startTransition(() => {
+      setTabIndex(newValue);
+    });
   };
 
   return (
     <>
       <ParametriPretrageSeminar onSubmit={handlePretraziSeminare} />
-
       <Tabs value={tabIndex} onChange={handleChange}>
         <Tab label="Pregled po seminaru" />
         <Tab label="Pregled po firmi" />
       </Tabs>
 
-      {tabIndex === 0 && (
-        <Box mt={2}>
-          <SeminariTable queryParameters={tableInputParameters} />
-        </Box>
-      )}
-
-      {tabIndex === 1 && <Box mt={2}></Box>}
+      <Box mt={2}>
+        {isPending ? (
+          <Box>
+            <Skeleton variant="rounded" animation="wave" />
+            <Skeleton variant="text" animation="wave" />
+            <Skeleton variant="rectangular" animation="wave" />
+            <Skeleton variant="text" animation="wave" />
+            <Skeleton variant="rectangular" animation="wave" />
+          </Box>
+        ) : (
+          <>
+            {tabIndex === 0 && (
+              <SeminariTable queryParameters={tableInputParameters} />
+            )}
+            {tabIndex === 1 && <Box>{/* Content for second tab */}</Box>}
+          </>
+        )}
+      </Box>
     </>
   );
 }
