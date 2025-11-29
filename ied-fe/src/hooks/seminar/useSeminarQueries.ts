5d05936f6bd84333ebea028eeadd428b53df2abd
Bogdan Raicevic <raicevic.bogdan@gmail.com>
2025-11-29 13:48:51 +0100

    cleanup

--- ied-fe/src/hooks/seminar/useSeminarQueries.ts
+++ ied-fe/src/hooks/seminar/useSeminarQueries.ts
@@ -2,17 +2,6 @@ import { useQuery } from "@tanstack/react-query";
 import type { SeminarQueryParams } from "ied-shared/dist/types/seminar.zod";
 import { fetchFirmaSeminari, fetchSeminari } from "../../api/seminari.api";
 
-export function useFetchFirmaSeminari() {
-  const { data: fetchedFirmaSeminars } = useQuery({
-    queryKey: ["firma-seminari"],
-    queryFn: () => {
-      return fetchFirmaSeminari(50, 0, {});
-    },
-  });
-
-  return { fetchedFirmaSeminars };
-}
-
 export function useSearchSeminari(params: {
   pageSize: number;
   pageIndex: number;
@@ -29,15 +18,12 @@ export function useSearchSeminari(params: {
 export function useSearchFirmaSeminari(params: {
   pageSize: number;
   pageIndex: number;
-  queryParameters: SeminarQueryParams;
+  queryParameters: SeminarQueryParams; // TODO: use different type if needed
 }) {
   const { pageSize, pageIndex, queryParameters } = params;
 
   const { data: firmaSeminars, isLoading } = useQuery({
-    queryKey: [
-      "firma-seminari",
-      { pageSize, pageIndex, filters: queryParameters },
-    ],
+    queryKey: ["firma-seminari", { pageSize, pageIndex, queryParameters }],
     queryFn: () => fetchFirmaSeminari(pageSize, pageIndex, queryParameters),
   });
 
