71d6f550722bf2e394b98fe989ff1c965b295fa1
Bogdan Raicevic <raicevic.bogdan@gmail.com>
2025-12-22 16:34:41 +0100

    Update form to be consistent with other ui elements

--- ied-fe/src/components/Seminari/ParametriPretrageFirmaSeminar.tsx
+++ ied-fe/src/components/Seminari/ParametriPretrageFirmaSeminar.tsx
@@ -1,5 +1,6 @@
 import type { FirmaSeminarSearchParams } from "@ied-shared/types/seminar.zod";
-import { Button, Grid, TextField } from "@mui/material";
+import { UnfoldLess } from "@mui/icons-material";
+import { Box, Button, FormControl, Grid, TextField } from "@mui/material";
 import { DatePicker } from "@mui/x-date-pickers";
 import { addMonths } from "date-fns";
 import { Controller, useForm } from "react-hook-form";
@@ -37,51 +38,52 @@ export default function ParametriPretrageFirmaSeminar({
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <Grid container spacing={2}>
-        <Grid size={3}>
-          <TextField
-            {...register("nazivFirme")}
-            fullWidth
-            label="Naziv firme"
-          />
-        </Grid>
-        <Grid size={3}>
-          <TextField
-            {...register("nazivSeminara")}
-            fullWidth
-            label="Naziv seminara"
-          />
-        </Grid>
-        <Grid size={3}>
-          <Controller
-            name="tipFirme"
-            control={control}
-            render={({ field }) => (
-              <AutocompleteMultiple
-                placeholder="Tip Firme"
-                data={tipoviFirme}
-                checkedValues={field.value}
-                onCheckedChange={field.onChange}
-                id={"tip-firme"}
-              />
-            )}
-          />
-        </Grid>
-        <Grid size={3}>
-          <Controller
-            name="delatnost"
-            control={control}
-            render={({ field }) => (
-              <AutocompleteMultiple
-                placeholder="Delatnost firme"
-                data={delatnosti}
-                checkedValues={field.value}
-                onCheckedChange={field.onChange}
-                id={"delatnost-firme"}
-              />
-            )}
-          />
-        </Grid>
-        {/* <Grid size={3}>
+        <Grid size={9} columns={12} container spacing={3}>
+          <Grid size={4}>
+            <TextField
+              {...register("nazivFirme")}
+              fullWidth
+              label="Naziv firme"
+            />
+          </Grid>
+          <Grid size={4}>
+            <TextField
+              {...register("nazivSeminara")}
+              fullWidth
+              label="Naziv seminara"
+            />
+          </Grid>
+          <Grid size={4}>
+            <Controller
+              name="tipFirme"
+              control={control}
+              render={({ field }) => (
+                <AutocompleteMultiple
+                  placeholder="Tip Firme"
+                  data={tipoviFirme}
+                  checkedValues={field.value}
+                  onCheckedChange={field.onChange}
+                  id={"tip-firme"}
+                />
+              )}
+            />
+          </Grid>
+          <Grid size={4}>
+            <Controller
+              name="delatnost"
+              control={control}
+              render={({ field }) => (
+                <AutocompleteMultiple
+                  placeholder="Delatnost firme"
+                  data={delatnosti}
+                  checkedValues={field.value}
+                  onCheckedChange={field.onChange}
+                  id={"delatnost-firme"}
+                />
+              )}
+            />
+          </Grid>
+          {/* <Grid size={4}>
           <Controller
             name="radnaMesta"
             control={control}
@@ -96,83 +98,87 @@ export default function ParametriPretrageFirmaSeminar({
             )}
           />
         </Grid> */}
-        <Grid size={3}>
-          <Controller
-            name="velicineFirme"
-            control={control}
-            render={({ field }) => (
-              <AutocompleteMultiple
-                placeholder="Veličina firme"
-                data={velicineFirme}
-                checkedValues={field.value}
-                onCheckedChange={field.onChange}
-                id={"velicina-firme-fs"}
-              />
-            )}
-          />
-        </Grid>
-        <Grid size={3}>
-          <Controller
-            name="predavac"
-            control={control}
-            render={({ field }) => (
-              <TextField
-                {...field}
-                fullWidth
-                label="Predavač"
-                onChange={(e) => field.onChange(e.target.value)}
-              />
-            )}
-          />
-        </Grid>
-        <Grid size={3}>
-          <Controller
-            name="tipSeminara"
-            control={control}
-            render={({ field }) => (
-              <MultiSelectAutocomplete
-                labelKey={"tipSeminara" as any}
-                options={tipoviSeminara || []}
-                value={field.value || []}
-                onChange={(newValue) => field.onChange(newValue)}
-                inputLabel="Tip seminara"
-                inputPlaceholder={
-                  isLoading ? "Učitavanje..." : "Izaberite tipove"
-                }
-              />
-            )}
-          />
+          <Grid size={4}>
+            <Controller
+              name="velicineFirme"
+              control={control}
+              render={({ field }) => (
+                <AutocompleteMultiple
+                  placeholder="Veličina firme"
+                  data={velicineFirme}
+                  checkedValues={field.value}
+                  onCheckedChange={field.onChange}
+                  id={"velicina-firme-fs"}
+                />
+              )}
+            />
+          </Grid>
+          <Grid size={4}>
+            <Controller
+              name="predavac"
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  fullWidth
+                  label="Predavač"
+                  onChange={(e) => field.onChange(e.target.value)}
+                />
+              )}
+            />
+          </Grid>
+          <Grid size={4}>
+            <Controller
+              name="tipSeminara"
+              control={control}
+              render={({ field }) => (
+                <MultiSelectAutocomplete
+                  labelKey={"tipSeminara" as any}
+                  options={tipoviSeminara || []}
+                  value={field.value || []}
+                  onChange={(newValue) => field.onChange(newValue)}
+                  inputLabel="Tip seminara"
+                  inputPlaceholder={
+                    isLoading ? "Učitavanje..." : "Izaberite tipove"
+                  }
+                />
+              )}
+            />
+          </Grid>
         </Grid>
 
-        <Grid size={3}>
-          <Controller
-            name="datumOd"
-            control={control}
-            render={({ field }) => (
-              <DatePicker
-                format="yyyy/MM/dd"
-                label="Startni datum seminara"
-                value={field.value}
-                onChange={(date) => field.onChange(date)}
-                slotProps={{ textField: { fullWidth: true } }}
-              />
-            )}
-          />
-        </Grid>
-        <Grid size={3}>
-          <Controller
-            name="datumDo"
-            control={control}
-            render={({ field }) => (
-              <DatePicker
-                format="yyyy/MM/dd"
-                label="Krajnji datum seminara"
-                value={field.value}
-                onChange={(date) => field.onChange(date)}
-                slotProps={{ textField: { fullWidth: true } }}
-              />
-            )}
-          />
+        <Grid size={3} columns={12} container>
+          <FormControl fullWidth>
+            <Controller
+              name="datumOd"
+              control={control}
+              render={({ field }) => (
+                <DatePicker
+                  format="yyyy/MM/dd"
+                  label="Startni datum seminara"
+                  value={field.value}
+                  onChange={(date) => field.onChange(date)}
+                  slotProps={{ textField: { fullWidth: true } }}
+                />
+              )}
+            />
+            <Box display="flex" alignItems="center" justifyContent="center">
+              <UnfoldLess />
+            </Box>
+            <Controller
+              name="datumDo"
+              control={control}
+              render={({ field }) => (
+                <DatePicker
+                  format="yyyy/MM/dd"
+                  label="Krajnji datum seminara"
+                  value={field.value}
+                  onChange={(date) => field.onChange(date)}
+                  slotProps={{ textField: { fullWidth: true } }}
+                />
+              )}
+            />
+          </FormControl>
         </Grid>
 
         <Grid size={12} sx={{ mb: 2 }}>
