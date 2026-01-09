1f995c6b02f0bc7a9fe0808f4b1d11bbfbb8c08b
Bogdan Raicevic <raicevic.bogdan@gmail.com>
2026-01-09 11:44:30 +0100

    chore: cleanup

--- ied-fe/src/components/AuditChangesViewer/AuditOverviewDetails.tsx
+++ ied-fe/src/components/AuditChangesViewer/AuditOverviewDetails.tsx
@@ -17,31 +17,42 @@ import {
   populateMissingDays,
 } from "../../utils/audit-log.helpers";
 
+const headers = [
+  "Datum",
+  "Dan u nedelji",
+  "Novi unosi",
+  "Obrisani unosi",
+  "Ažurirani unosi",
+  "Najranija izmena",
+  "Najkasnija izmena",
+  "Procenjeno vreme rada",
+  "Prosečno vreme između unosa",
+  "Najveća pauza između unosa",
+];
+
 export default function AuditOverviewDetails({
   auditData,
 }: {
   auditData: AuditLogStatsByDateResponse;
 }) {
-  const headers = [
-    "Datum",
-    "Dan u nedelji",
-    "Novi unosi",
-    "Obrisani unosi",
-    "Ažurirani unosi",
-    "Najranija izmena",
-    "Najkasnija izmena",
-    "Procenjeno vreme rada",
-    "Prosečno vreme između unosa",
-    "Najveća pauza između unosa",
-  ];
-
   const updatedStats = populateMissingDays({
     dailyStats: auditData.dailyStats,
     dateStart: auditData.dateStart,
     dateEnd: auditData.dateEnd,
   });
 
-  const table = (
+  const getRowColor = (isWeekend: boolean, hasUpdates: boolean) => {
+    if (isWeekend) return grey[100];
+    if (hasUpdates) return green[50];
+    return red[50];
+  };
+  const getHoverColor = (isWeekend: boolean, hasUpdates: boolean) => {
+    if (isWeekend) return grey[200];
+    if (hasUpdates) return green[100];
+    return red[100];
+  };
+
+  return (
     <TableContainer component={Paper} sx={{ mt: 3, maxHeight: "80vh" }}>
       <Table sx={{ tableLayout: "fixed" }} stickyHeader>
         <TableHead>
@@ -56,25 +67,18 @@ export default function AuditOverviewDetails({
         <TableBody>
           {updatedStats.map((stat) => {
             const { dayName, isWeekend } = getDayInfo(stat.date);
-            const getRowColor = () => {
-              if (isWeekend) return grey[100];
-              if (stat.aggregatedUpdated > 0) return green[50];
-              return red[50];
-            };
-            const getHoverColor = () => {
-              if (isWeekend) return grey[200];
-              if (stat.aggregatedUpdated > 0) return green[100];
-              return red[100];
-            };
+            const colors = getRowColor(isWeekend, stat.aggregatedUpdated > 0);
+            const hoverColors = getHoverColor(
+              isWeekend,
+              stat.aggregatedUpdated > 0,
+            );
 
             return (
               <TableRow
                 key={stat.date}
                 sx={{
-                  backgroundColor: getRowColor(),
-                  "&:hover": {
-                    backgroundColor: getHoverColor(),
-                  },
+                  backgroundColor: colors,
+                  "&:hover": { backgroundColor: hoverColors },
                 }}
               >
                 <TableCell>{stat.date}</TableCell>
@@ -100,6 +104,4 @@ export default function AuditOverviewDetails({
       </Table>
     </TableContainer>
   );
-
-  return <Box>{table}</Box>;
 }
