38b1455c80cc1310ba63e543c0dab7166701440c
Bogdan Raicevic <raicevic.bogdan@gmail.com>
2026-01-09 11:04:24 +0100

    Improve coloring on the audit overview table

--- ied-fe/src/components/AuditChangesViewer/AuditOverviewDetails.tsx
+++ ied-fe/src/components/AuditChangesViewer/AuditOverviewDetails.tsx
@@ -8,7 +8,7 @@ import {
   TableHead,
   TableRow,
 } from "@mui/material";
-import { grey } from "@mui/material/colors";
+import { green, grey, red } from "@mui/material/colors";
 import type { AuditLogStatsByDateResponse } from "ied-shared";
 import {
   getDayInfo,
@@ -54,13 +54,24 @@ export default function AuditOverviewDetails({
         <TableBody>
           {updatedStats.map((stat) => {
             const { dayName, isWeekend } = getDayInfo(stat.date);
+            const getRowColor = () => {
+              if (isWeekend) return grey[100];
+              if (stat.aggregatedUpdated > 0) return green[50];
+              return red[50];
+            };
+            const getHoverColor = () => {
+              if (isWeekend) return grey[200];
+              if (stat.aggregatedUpdated > 0) return green[100];
+              return red[100];
+            };
+
             return (
               <TableRow
                 key={stat.date}
                 sx={{
-                  backgroundColor: isWeekend ? grey[100] : "transparent",
+                  backgroundColor: getRowColor(),
                   "&:hover": {
-                    backgroundColor: isWeekend ? grey[200] : grey[50],
+                    backgroundColor: getHoverColor(),
                   },
                 }}
               >
