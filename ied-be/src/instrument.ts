2a7d43ac2227ee255471d5e47a695aba96113d08
Bogdan Raicevic <raicevic.bogdan@gmail.com>
2026-03-20 21:46:54 +0100

    feat: include sentry in the start and dev script

--- ied-be/src/instrument.ts
+++ ied-be/src/instrument.ts
@@ -1,11 +1,15 @@
 import * as Sentry from "@sentry/node";
 import { env } from "./utils/envVariables";
 
+const dsn = env.sentry.dsn;
+const isProduction = env.nodeEnv === "production";
+
 Sentry.init({
-  dsn: env.sentry.dsn,
-  tracesSampleRate: env.nodeEnv === "development" ? 1.0 : 0.1,
+  dsn: dsn || undefined,
+  enabled: Boolean(dsn),
+  tracesSampleRate: isProduction ? 1.0 : 0.1,
   sendDefaultPii: true,
   includeLocalVariables: true,
   enableLogs: true,
-  serverName: env.serverName,
+  serverName: env.serverName || "development",
 });
