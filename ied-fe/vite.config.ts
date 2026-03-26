55cdbf5bf0feb38ded4d785f523144f07c490785
Bogdan Raicevic <raicevic.bogdan@gmail.com>
2026-03-26 14:48:39 +0100

    fix: use correct react plugin for vite configuration

--- ied-fe/vite.config.ts
+++ ied-fe/vite.config.ts
@@ -1,9 +1,10 @@
 import path from "node:path";
+import react from "@vitejs/plugin-react";
 import { defineConfig, searchForWorkspaceRoot } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [],
+  plugins: [react()],
   build: {
     outDir: "dist",
     sourcemap: false,
