595e629ae5bfbb79765728931e3cdbb36b0a9434
Bogdan Raicevic <raicevic.bogdan@gmail.com>
2026-04-22 13:25:26 +0200

    feat: add IzdavacBadge component to display issuer logo in SummaryPanel

--- ied-fe/src/components/RacunV2/RacunV2FormProvider.tsx
+++ ied-fe/src/components/RacunV2/RacunV2FormProvider.tsx
@@ -1,10 +1,5 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import {
-  type RacunV2Form,
-  type RacunV2Parsed,
-  RacunV2Zod,
-  TipRacuna,
-} from "ied-shared";
+import { type RacunV2Form, RacunV2Zod, TipRacuna } from "ied-shared";
 import type { ReactNode } from "react";
 import { FormProvider, useForm } from "react-hook-form";
 import { getDefaultValues } from "./schema/defaults";
