3d18aef9a09b1069ee9df7f65cda24afa2728853
Bogdan Raicevic <raicevic.bogdan@gmail.com>
2026-08-22 22:02:02 +0200

    remove dead code

--- ied-fe/src/pages/Pretrage.tsx
+++ ied-fe/src/pages/Pretrage.tsx
@@ -3,13 +3,6 @@ import { FirmasTable } from "../components/FirmasTable";
 import PageTitle from "../components/PageTitle";
 import PredefinedPretrage from "../components/PredefinedPretrage/PredefinedPretrage";
 import PretragaParameters from "../components/PretragaParameters/PretragaParameters";
-import { env } from "../utils/envVariables";
-
-const PUBLISHABLE_KEY = env.clerkPublishableKey;
-
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key");
-}
 
 export default function Pretrage() {
   return (
